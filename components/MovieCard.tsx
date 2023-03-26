1dc7a52a983c8133f478bace66ff473d99a5b4b0
Nguyễn Hoàng Hải <ln26805@gmail.com>
2023-03-27 01:04:08 +0700

    playMovie

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Image from "next/image";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
+import { useRouter } from "next/router";
 
 interface MovieCardProps {
   data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const router = useRouter();
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <Image
@@ -57,7 +59,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         "
         >
           <div className="flex flex-row items-center gap-3">
-            <div className="cursor-pointer w-16 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
+            <div
+              onClick={() => router.push(`/watch/${data?.id}`)}
+              className="cursor-pointer w-16 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
+            >
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
